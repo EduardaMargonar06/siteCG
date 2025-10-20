18b1654d109f37f93fdd99b93ec97ef5e66296e9
Margo <131906487+EduardaMargonar06@users.noreply.github.com>
2025-10-20 15:43:32 -0300

    cedv

--- Script/header.js
+++ Script/header.js
@@ -1,14 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const headerHTML = `
+document.addEventListener("DOMContentLoaded", function () { 
+  const linkCSS = document.createElement("link");
+  linkCSS.rel = "stylesheet";
+  linkCSS.href = "CSS/style.css";
+  document.head.appendChild(linkCSS);
+    const headerHTML = `
   <header class="header">
-    <nav class="menu_topo_CG">
         <div class="CG_menu">
-            <a href="MainCG.html">
-                <img class="logo_CG" src="Imagens/CG-titulo.png" alt="Logo">
-            </a>
+            <img src="Imagens/Ratinha_CG.png" class="pitchula">
+            <img class="logo" src="Imagens/CG-titulo.png" alt="Logo">
             <nav class="menu_itens_CG">
                 <div class="secao_menu_CG">
                     <nav class="secundario_menu_CG_1">
+                        <a class="secundario_link" href="index.html">Início</a>
                         <a class="secundario_link" href="sobreNos.html">Sobre nós</a>
                         <a class="secundario_link" href="galeria_cg.html">Mídia</a>
                         <a class="secundario_link" href="CG_mulheres.html">Mulheres na Computação</a>
@@ -17,9 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             </nav>
         </div>
-    </nav>
   </header>
   `;
 
-  document.body.insertAdjacentHTML("afterbegin", headerHTML);
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
 });
