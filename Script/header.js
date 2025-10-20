62ea65242b9d744deaae7adb957e3fa34ea3d27c
Margo <131906487+EduardaMargonar06@users.noreply.github.com>
2025-10-20 18:46:04 -0300

    ce

--- Script/header.js
+++ Script/header.js
@@ -5,17 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     document.head.appendChild(linkCSS);
     const headerHTML = `
   <header class="header">
-        <div class="CG_menu">
-            <img src="Imagens/Ratinha_CG.png" class="pitchula">
-            <img class="logo" src="Imagens/CG-titulo.png" alt="Logo">
-                <nav class="menu">                        
-                    <a class="link" href="index.html">Início</a>
-                    <a class="link" href="sobreNos.html">Sobre nós</a>
-                    <a class="link" href="galeria_cg.html">Mídia</a>
-                    <a class="link" href="CG_mulheres.html">Mulheres na Computação</a>
-                    <a class="link" href="contatos_CG.html">Contatos</a>
-                </nav>
-        </div>
+        <img src="Imagens/Ratinha_CG.png" class="pitchula">
+        <img class="logo" src="Imagens/CG-titulo.png" alt="Logo">
+        <nav class="menu">                        
+            <a class="link" href="index.html">Início</a>                    
+            <a class="link" href="sobreNos.html">Sobre nós</a>
+            <a class="link" href="galeria_cg.html">Mídia</a>
+            <a class="link" href="CG_mulheres.html">Mulheres na Computação</a>
+            <a class="link" href="contatos_CG.html">Contatos</a>
+        </nav>
   </header>
   `;
 
